7662d835f2a3de9fff4a57642b201c7b79539fe2
6871 <55576043+6871@users.noreply.github.com>
2021-06-01 21:04:51 +0100

    Add function epoch_seconds_from_date_string

--- bash/time_utils.sh
+++ bash/time_utils.sh
@@ -6,7 +6,7 @@
 # second arguments to stdout; if no arguments are given the current epoch time
 # is used.
 #
-# Ubunutu Docker container non UTC support may require:
+# Ubuntu Docker container non UTC support may require:
 #   apt-get update
 #   export DEBIAN_FRONTEND=noninteractive
 #   apt-get install --yes tzdata
@@ -39,4 +39,38 @@ function epoch_seconds() {
         "$(date -d @"${epoch_time}")"
     fi
   done
+}
+
+# macOS   : date -u -j -f '%Y-%m-%d %H:%M:%S' '2021-06-01 16:35:16'
+# macOS   : date -u -j -f '%Y-%m-%d %H:%M:%S' '2021-06-01 16:35:16' +'%s'
+#         : 1622565316
+# Ubuntu  : TZ=UTC date -d '2021-06-01 16:35:16' +'%s'
+#         : 1622565316
+function epoch_seconds_from_date_string() {
+  if [ $# -ne 1 ]; then
+    printf 'Usage                   : date_string\n'
+    printf 'macOS example, UTC only : %s\n' "'2021-06-01 16:35:16'"
+    printf 'Linux examples          : %s\n' "'2021-06-01 16:35:16'"
+    printf '                        : %s\n' "'2021-06-01 16:35:16 BST'"
+    printf '                        : %s\n' "'2021-06-01 16:35:16 PST'"
+    return 1
+  fi
+
+  local date_string="${1}"
+
+  if [[ $(uname) == 'Darwin' ]]; then
+    local format_string='%Y-%m-%d %H:%M:%S'
+
+    printf '%s : %s : %s\n' \
+      "${date_string}" \
+      "$(date -u -j -f "${format_string}" "${date_string}")" \
+      "$(date -u -j -f "${format_string}" "${date_string}" +'%s')"
+  else
+    local time_zone='UTC'
+
+    printf '%s : %s : %s\n' \
+      "${date_string}" \
+      "$(TZ="${time_zone}" date -d "${date_string}")" \
+      "$(TZ="${time_zone}" date -d "${date_string}" +'%s')"
+  fi
 }
